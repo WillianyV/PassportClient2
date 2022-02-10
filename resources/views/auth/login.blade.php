3ef41e37b6d638d843696a45fcd80f2758daff6f
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-02-10 11:34:04 -0300

    melhorando o client

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="d-grid gap-2 col-12 mx-auto">
-                <a href="{{ route("sso.redirect") }}" class="btn btn-danger btn-sm">Login</a>
+                <a href="{{ route("sso.redirect") }}" class="btn btn-danger btn-sm" id="id_login">Login</a>
             </div>            
         </div>
     </div>
